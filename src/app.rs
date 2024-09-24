ceced6a9559702a74587cffd08b9b3ec15fcbaf0
Asyraf <asyzruffz@gmail.com>
2024-09-24 22:18:05 +0800

    Add egui sample

---
+++ src/app.rs
@@ -0,0 +1,32 @@
+use eframe::egui;
+
+pub struct App;
+
+impl App {
+    pub fn run(title: &str) -> eframe::Result {
+        let options = eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+            ..Default::default()
+        };
+
+        // Our application state:
+        let mut name = "Arthur".to_owned();
+        let mut age = 42;
+
+        eframe::run_simple_native(title, options, move |ctx, _frame| {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.heading("My egui Application");
+                ui.horizontal(|ui| {
+                    let name_label = ui.label("Your name: ");
+                    ui.text_edit_singleline(&mut name)
+                        .labelled_by(name_label.id);
+                });
+                ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+                if ui.button("Increment").clicked() {
+                    age += 1;
+                }
+                ui.label(format!("Hello '{name}', age {age}"));
+            });
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use bunyi::app::App;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let _ = App::run("Bunyi");
 }
